192b5647fa0e6bfa4bfa3699219fc92776c4cea3
CLEMDEF <clementdefombelle@gmail.com>
2025-04-04 10:55:20 -0400

    update typing_extensions version requirements

--- shiradl/tagging.py
+++ shiradl/tagging.py
@@ -6,7 +6,7 @@ from io import BytesIO
 from pathlib import Path
 from statistics import mean, stdev
 import sys
-if sys.version_info < (3, 10):
+if sys.version_info < (3, 11):
 	from typing_extensions import NotRequired, TypedDict
 else:
 	from typing import NotRequired, TypedDict
